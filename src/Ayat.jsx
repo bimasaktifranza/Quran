b672477d82299fbeea4e74a9ba58fc7573a8fc8a
bimasaktifranza <12350111622@students.uin-suska.ac.id>
2025-03-16 20:09:05 +0700

    Commit

--- src/Ayat.jsx
+++ src/Ayat.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams, Link } from "react-router";
+import { useParams, Link } from "react-router-dom";
 
 function Ayat() {
     const { nomor } = useParams();
